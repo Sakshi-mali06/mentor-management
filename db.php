7636262d4fd290610dada590811e7d7f407d84cb
Sakshi-mali06 <sakshimali54806@gmail.com>
2026-09-04 23:32:14 +0530

    db.php

--- db.php
+++ db.php
@@ -1,7 +1,11 @@
-
 <?php
 
-$conn = mysqli_connect("localhost", "root", "", "mentor_db");
+$host = sql300.infinityfree.com;
+$user = if0_42834178;
+$password = 2jlzZ7ik0N;
+$database = if0_42834178_XXX;
+
+$conn = mysqli_connect($host, $user, $password, $database);
 
 if (!$conn) {
     die("Connection failed");
